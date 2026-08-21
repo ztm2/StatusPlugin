4d5e72b49f01a1b896e601c99dd965922ace8cc6
ztm2 <udon0407@gmail.com>
2026-08-22 01:13:58 +0900

    fix:万が一テキストディスプレイが降りてしまったときのための対策

--- src/main/java/com/ztm2/status/Status.java
+++ src/main/java/com/ztm2/status/Status.java
@@ -6,14 +6,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.TextDisplay;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDismountEvent;
 import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.MountInventory;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+
+import static com.ztm2.status.Utils.STATUS_OWNER_KEY;
 
 public final class Status extends JavaPlugin implements Listener {
 
@@ -58,5 +62,10 @@ public final class Status extends JavaPlugin implements Listener {
         if(textDisplay == null) return;
         if(e.getPlayer().getGameMode() == GameMode.SPECTATOR) textDisplay.setViewRange(1.0f);
         if(e.getNewGameMode() == GameMode.SPECTATOR) textDisplay.setViewRange(0.0f);
+    }
+
+    @EventHandler
+    public void onEntityDismount(EntityDismountEvent e) {
+        if(e.getEntity().getPersistentDataContainer().has(STATUS_OWNER_KEY)) e.setCancelled(true);
     }
 }
